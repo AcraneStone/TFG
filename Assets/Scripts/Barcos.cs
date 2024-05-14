2ab9c408744e7f056ad54ea7c77300c0d1c40895
Eva de la Muela Marín <edlm1@alu.ua.es>
2024-05-14 16:55:12 +0200

    commit inicial

---
+++ Assets/Scripts/Barcos.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nuevo Barco", menuName = "ScriptableObjects/Barco")]
+public class Barcos : ScriptableObject
+{
+    // Start is called before the first frame update
+    public int artilleria;
+    public int soldados;
+    public int  tripulantes;
+    public float velocidad1, velocidad2, velocidadMax;
+    public GameObject carcasa;
+    public float vida;
+    public float vidaTotal;
+    public float fuerza;
+
+    public float perdidasTripulantes;
+    public float perdidasSoldados;
+    //artilleria???
+    [TextArea(2, 2)]
+    public string descripcion;
+    public int capacity;
+    public bool activo;//para el puerto
+    //InventoryProductSO inventario;
+
+    
+}
+
